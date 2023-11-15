dc455a2148fc6a69f874867e9a10d1f97570ace4
Sibiselva <63643348+Sibiselva-N@users.noreply.github.com>
2023-11-16 02:15:33 +0530

    f

---
+++ webapp/urls.py
@@ -0,0 +1,32 @@
+from django.urls import path
+from .views import show_dashboard, show_category, show_service, show_banner, show_district, edit_district, add_district, \
+    show_user, \
+    add_service, edit_service, add_category, edit_category, delete_category, add_banner, edit_banner, delete_service, \
+    delete_banner, delete_district, delete_grade, edit_grade, add_grade, show_grade
+
+urlpatterns = [
+    path('dashboard', show_dashboard),
+    path('category', show_category),
+    path('category_add', add_category),
+    path('category_edit', edit_category, name='category_edit'),
+    path('category_delete', delete_category, name='category_delete'),
+    path('service', show_service),
+    path('banner', show_banner),
+    path('banner_add', add_banner),
+    path('banner_edit', edit_banner, name='banner_edit'),
+    path('banner_delete', delete_banner, name='banner_delete'),
+    path('district', show_district),
+    path('district_add', add_district),
+    path('district_edit', edit_district, name='district_edit'),
+    path('district_delete', delete_district, name='district_delete'),
+
+    path('grade', show_grade),
+    path('grade_add', add_grade),
+    path('grade_edit', edit_grade, name='grade_edit'),
+    path('grade_delete', delete_grade, name='grade_delete'),
+
+    path('users', show_user),
+    path('service_add', add_service),
+    path('service_edit', edit_service, name='service_edit'),
+    path('service_delete', delete_service, name='service_delete')
+]
---
+++ webapp/views.py
@@ -0,0 +1,484 @@
+import json
+import time
+
+import firebase_admin
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from firebase_admin import credentials, firestore, storage
+
+json_key = {
+    "type": "service_account",
+    "project_id": "edhunt",
+    "private_key_id": "f9d4ffc352178ab32ef126ded89eabedb85f414a",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCkjjKUJAmyODBW\n/t/wGxh/plsJNZnIqzR+t0mit1hne6DXcgB4DKS8s7XKe3p8HS/V/XmSliafSAeB\nacVit5P43WRlPc2bDzmxd4b0bsS7CZkDYc8PcSWQbmhkGvUdJYfSFoW6H+dmRqGC\nc3v3CoZTtAqF+2zQ9egzlhEizpM2XymUdABpvvAwYM9kDI6mVKI5Tc7u3PgsXPJQ\nrouyGpE9joWysWI9KQoFpJRyL6jXH81SpNuTptAxWRWismYwwmQXHo0fYs3zzUSB\nmCWJgMTE7Cus4QZl4kXIb41bwIo/5EUEonbFqHdLohWebSauqE5gQOLJEaIpLzxY\no3Tb+H37AgMBAAECggEADgHGn2KD6sYqozUVxb/gsXjtFdcYrIKaONEMNQwLO9/D\nnKMpFA7rdN6NMluZXPg1Cq+FSit6xSMh9GuW8CS1NUFPGukMV88Pd6I3fBXzrAIQ\nor+QGv3kFyu2uhKrW5xGt+TP5Dxz+3o2pD6FRk2Aip5wKOBybkUIS1/SPJa95CpJ\nh1DD+5HV1+09blUTIJY/LhqSoIRxc/DA4XwyRTDywDi4VmFOpyt7aw9gLkY53iWg\nhzROaiH7J/cq423JY2fLCyGwMgzQQVIcIcd8FQWVuzifwE7LHxC4pxC2c7d66u/P\niefRq/0oIjVWHnblSnGiGpGKaZhM5MVZvrZlYms+vQKBgQDYkZxUJJGPtNn9hxWW\nXFsZTS17zxrdOLBrw2GNrjGH12uaGNcje9uuKYLObSuVlTeEJbC5ll5HVkAOyZmO\noLPVXHhMa2Z56Wxa/YoHORkQNV+1lhcZnmm/eEeMA2rtwA6h/vo+F+WgD6USyPrO\nSNFNwCWWEiz4E+bsjAt17NcM1QKBgQDChDeX4ST1wJClmTUJ4WtI+xkQPAvmBcSs\nFFR17qcp4bKlhYoSraek8EOs7v4mGe/fZ+J1oKiAUprJ7IvruQda5jY4FnF4/8i3\n9Y9UU624+aGSebQhVIua55+7kmOULI5HOzHjpwvHLcleiDZa0H4oA1xxKw4ggA7y\nqV3HgcSHjwKBgAwI+4EJjzVHPZ4DNcXnWACt80sgFUQZ9GPotbEj7wi3hflITBxi\ns7CYCfbixjtH0Y/8cDfADXk1Z9XqiigM8jF3NBA7H7TCrgzYbiU8nRHzhWAX+Syp\nSwMi8gbr4bNYqveBrfJpbY9ZjzjeBmIUVd1WQkB2vh0DLiATIEfrY4AJAoGALb2P\noRxlZ5CvsvEzAq7KXrAFNccY+S80D078iXrkPjn/m2KqlDfXqzaAg4LzqpwAxyUT\noJt15C8IjSEdygGJlDt8VnYYmt5zIacR3D+NI2k4MEyEKi5KYdPDwQTse6R6lI97\n7M+UOnMu3hbjyTkr4Vbg/w+j08qpOTDQh6ChSUsCgYB7PoZIRM+qq427GUnD80qB\nVr+4yNCkHoKygkFjnKchQibIaCKVMjDda5rErT5rexEVT6SRHm8UhSyZ/R8U1XIV\nvD5hii7CiEyj0jbPtC+F3e2zkDSd9bV/ok6N2HcnPm3aaQxCt6zVzqL8BIgU/zJ4\nqUXzeDwq/ooY7uNDpA+ZKg==\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-ebkth@edhunt.iam.gserviceaccount.com",
+    "client_id": "114363773335303361188",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ebkth%40edhunt.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+}
+cred = credentials.Certificate(json_key)
+firebase_admin.initialize_app(cred, {'storageBucket': 'edhunt.appspot.com'})
+bucket = storage.bucket()
+
+
+def show_dashboard(request):
+    return render(request, 'index.html')
+
+
+def show_category(request):
+    fs = firestore.client()
+    cat_doc = fs.collection("indian_category").get()
+    category = []
+    for i in cat_doc:
+        d = i.to_dict()
+        d['id'] = i.id
+        category.append(d)
+    return render(request, 'category.html', {"cat": category})
+
+
+def add_category(request):
+    if request.method == "POST":
+        data = request.POST
+        if 'name' in data and 'coach' in data and 'distance' in data and 'indian' in data:
+            db = firestore.client()
+            d = db.collection('indian_category').document()
+            d.set({
+                "id": d.id,
+                "isCoaching": str(data['coach']).lower() == "true",
+                "isDistance": str(data['distance']).lower() == "true",
+                "isIndian": str(data['indian']).lower() == "true",
+                "name": data['name']
+            })
+            return HttpResponseRedirect('category')
+        else:
+            print("no fields")
+            return HttpResponseRedirect('category')
+    else:
+        return render(request, 'category-add.html')
+
+
+def edit_category(request):
+    if request.method == "POST":
+        data = request.POST
+        if 'name' in data and 'coach' in data and 'id' in data and 'distance' in data and 'indian' in data:
+            db = firestore.client()
+            db.collection('indian_category').document(data['id']).update({
+                "id": data['id'],
+                "isCoaching": str(data['coach']).lower() == "true",
+                "isDistance": str(data['distance']).lower() == "true",
+                "isIndian": str(data['indian']).lower() == "true",
+                "name": data['name']
+            })
+            return HttpResponseRedirect('category')
+        else:
+            return HttpResponseRedirect('category')
+    else:
+        cat_id = request.GET.get('id', "")
+        db = firestore.client()
+        details = db.collection('indian_category').document(cat_id).get()
+        cat = details.to_dict()
+    return render(request, 'category-edit.html', {"cat": cat, "cat_id": cat_id})
+
+
+def delete_category(request):
+    data = request.POST
+    cat_id = data['id']
+    db = firestore.client()
+    db.collection('indian_category').document(cat_id).delete()
+    cat_doc = db.collection("indian_category").get()
+    category = []
+    for i in cat_doc:
+        d = i.to_dict()
+        d['id'] = i.id
+        category.append(d)
+    return render(request, 'category.html', {"cat": category})
+
+
+def show_service(request):
+    fs = firestore.client()
+    ser_coll = fs.collection('indian_college').get()
+    dist = fs.collection('district').get()
+    services = []
+    for i in ser_coll:
+        di_name = ""
+        d = i.to_dict()
+        for j in dist:
+            if j.id == d['district_id']:
+                di_name = j.to_dict()['name']
+
+        d['district'] = di_name
+        services.append(d)
+    return render(request, 'services.html', {"services": services})
+
+
+def add_service(request):
+    data = request.POST
+    if request.method == "POST":
+        data_dict = dict(data)
+        for key, value in data_dict.items():
+            if len(value) == 1 and value[0] == "[]":
+                data_dict[key] = []
+            else:
+                data_dict[key] = value[0]
+        photos = request.FILES.getlist('image')
+        pdf = request.FILES.getlist('pdf_url')
+        data = data_dict
+        if 'about' in data and 'category_id' in data and 'count' in data and "district_id" in data and "grade_id" in data and "isAutonomous" in data and "isCoaching" in data and "isDistance" in data and "isIndian" in data and "name" in data and "number" in data and 'university' in data:
+            url_list = []
+            pdf_url = ""
+            for i in photos:
+                blob = bucket.blob(i.name)
+                blob.upload_from_file(i.file)
+                blob.make_public()
+                url_list.append(blob.public_url)
+            if len(pdf) == 1:
+                blob = bucket.blob(pdf[0].name)
+                blob.upload_from_file(pdf[0].file)
+                blob.make_public()
+                pdf_url = blob.public_url
+            db = firestore.client()
+            d = db.collection('indian_college').document()
+            d.set({
+                "about": data['about'],
+                "category_id": data['category_id'],
+                "count": int(data['count']),
+                "image": url_list,
+                "district_id": data['district_id'],
+                "grade_id": data['grade_id'],
+                "isAutonomous": str(data['isAutonomous']).lower() == "true",
+                "isCoaching": str(data['isCoaching']).lower() == "true",
+                "isDistance": str(data['isDistance']).lower() == "true",
+                "isIndian": str(data['isIndian']).lower() == "true",
+                "university": data['university'],
+                "name": data['name'],
+                "number": data['number'],
+                "pdf_url": pdf_url,
+                "id": d.id
+            })
+            return HttpResponseRedirect('service')
+        else:
+            return ""
+    fs = firestore.client()
+    cat_doc = fs.collection("indian_category").get()
+    category = []
+    cities = []
+    grade = []
+    for i in cat_doc:
+        category.append(i.to_dict())
+    city_doc = fs.collection('district').get()
+    for i in city_doc:
+        cities.append(i.to_dict())
+    grade_doc = fs.collection('grade').get()
+    for i in grade_doc:
+        grade.append(i.to_dict())
+    return render(request, 'service-add.html', {
+        'category': category,
+        'city': cities,
+        'grade': grade,
+    })
+
+
+def edit_service(request):
+    data = request.POST
+    if request.method == "POST":
+        data_dict = dict(data)
+        for key, value in data_dict.items():
+            if len(value) == 1 and value[0] == "[]":
+                data_dict[key] = []
+            elif key == "description":
+                data_dict[key] = json.loads(value[0])
+            elif key == "old_url":
+                result = json.loads(value[0])
+                result_list = [str(item) for item in result]
+                data_dict[key] = result_list
+            else:
+                data_dict[key] = value[0]
+        photos = request.FILES.getlist('image')
+        pdf = request.FILES.getlist('pdf_url')
+        data = data_dict
+        if 'about' in data and 'category_id' in data and 'count' in data and "district_id" in data and "grade_id" in data and "isAutonomous" in data and "isCoaching" in data and "isDistance" in data and "isIndian" in data and "name" in data and "number" in data and 'university' in data and 'id' in data and 'old_url' in data:
+            url_list = []
+            for i in data['old_url']:
+                if type(i) is str:
+                    url_list.append(i)
+            pdf_url = ""
+            db = firestore.client()
+            for i in photos:
+                blob = bucket.blob(i.name)
+                blob.upload_from_file(i.file)
+                blob.make_public()
+                url_list.append(blob.public_url)
+            if len(pdf) == 1:
+                blob = bucket.blob(pdf[0].name)
+                blob.upload_from_file(pdf[0].file)
+                blob.make_public()
+                pdf_url = blob.public_url
+                db.collection('indian_college').document(data['id']).update({
+                    "pdf_url": pdf_url,
+                })
+
+            db.collection('indian_college').document(data['id']).update({
+                "about": data['about'],
+                "category_id": data['category_id'],
+                "count": int(data['count']),
+                "image": url_list,
+                "district_id": data['district_id'],
+                "grade_id": data['grade_id'],
+                "isAutonomous": str(data['isAutonomous']).lower() == "true",
+                "isCoaching": str(data['isCoaching']).lower() == "true",
+                "isDistance": str(data['isDistance']).lower() == "true",
+                "isIndian": str(data['isIndian']).lower() == "true",
+                "university": data['university'],
+                "name": data['name'],
+                "number": data['number'],
+                "id": data['id']
+            })
+            return HttpResponseRedirect('service')
+        else:
+            return ""
+    else:
+        clg_id = request.GET.get('id', "")
+        db = firestore.client()
+        service = db.collection('indian_college').document(clg_id).get()
+        cat_doc = db.collection("indian_category").get()
+        category = []
+        cities = []
+        grade = []
+        for i in cat_doc:
+            category.append(i.to_dict())
+        city_doc = db.collection('district').get()
+        for i in city_doc:
+            cities.append(i.to_dict())
+        grade_doc = db.collection('grade').get()
+        for i in grade_doc:
+            grade.append(i.to_dict())
+        ser = service.to_dict()
+        return render(request, 'service-edit.html', {
+            'category': category,
+            'city': cities,
+            'grade': grade,
+            'service': ser,
+            'service_id': clg_id,
+        })
+
+
+def delete_service(request):
+    data = request.POST
+    if request.method == "POST":
+        if 'id' in data and 'service_name' in data:
+            db = firestore.client()
+            db.collection('services').document(data['service_name']).set({"d": "d"})
+            db.collection('services').document(data['service_name']).collection(data['service_name']).document(
+                data['id']).delete()
+            return HttpResponseRedirect('service')
+        else:
+            return ""
+
+
+def show_banner(request):
+    fs = firestore.client()
+    ban_doc = fs.collection("banner").get()
+    ban = []
+    for i in ban_doc:
+        d = i.to_dict()
+        ban.append(d)
+    return render(request, 'banner.html', {"ban": ban})
+
+
+def add_banner(request):
+    if request.method == "POST":
+        data = request.POST
+        files = request.FILES.getlist('image')
+        if len(files) == 1:
+            ima = files[0]
+            blob = bucket.blob(ima.name + str(time.time_ns()) + ".png")
+            blob.upload_from_file(ima.file)
+            blob.make_public()
+            url = blob.public_url
+            db = firestore.client()
+            d = db.collection('banner').document()
+            d.set({
+                "id":d.id,
+                "url": url
+            })
+            return HttpResponseRedirect('banner')
+        else:
+            print("no fields")
+            return HttpResponseRedirect('banner')
+    else:
+        return render(request, 'banner-add.html')
+
+
+def edit_banner(request):
+    if request.method == "POST":
+        data = request.POST
+        if 'id' in data and 'old_url' in data:
+            ima = request.FILES.getlist('image')
+            if len(ima) == 1:
+                blob = bucket.blob(ima[0].name + str(time.time_ns()) + ".png")
+                blob.upload_from_file(ima[0].file)
+                blob.make_public()
+                url = blob.public_url
+            else:
+                url = data['old_url']
+            db = firestore.client()
+            db.collection('banner').document(data['id']).update({
+                "url": url,
+                "id":data['id']
+            })
+            return HttpResponseRedirect('banner')
+        else:
+            return HttpResponseRedirect('banner')
+    else:
+        ban_id = request.GET.get('id', "")
+        db = firestore.client()
+        details = db.collection('banners').document(ban_id).get()
+        cat = details.to_dict()
+    return render(request, 'banner-edit.html', {"ban": cat, "ban_id": ban_id})
+
+
+def delete_banner(request):
+    data = request.POST
+    cat_id = data['id']
+    db = firestore.client()
+    db.collection('banner').document(cat_id).delete()
+    cat_doc = db.collection("banner").get()
+    category = []
+    for i in cat_doc:
+        d = i.to_dict()
+        category.append(d)
+    return render(request, 'banner.html', {"cat": category})
+
+
+def show_district(request):
+    fs = firestore.client()
+    cat_doc = fs.collection("district").get()
+    city = []
+    for i in cat_doc:
+        d = i.to_dict()
+        d['id'] = i.id
+        city.append(d)
+    return render(request, 'district.html', {"city": city})
+
+
+def add_district(request):
+    if request.method == "POST":
+        data = request.POST
+        if 'name' in data:
+            db = firestore.client()
+            db.collection('district').document().set({
+                "name": data['name']
+            })
+            return HttpResponseRedirect('district')
+        else:
+            print("no fields")
+            return HttpResponseRedirect('district')
+    else:
+        return render(request, 'district-add.html')
+
+
+def edit_district(request):
+    if request.method == "POST":
+        data = request.POST
+        if 'name' in data and 'id' in data:
+            db = firestore.client()
+            db.collection('district').document(data['id']).update({
+
+                "name": data['name']
+            })
+            return HttpResponseRedirect('district')
+        else:
+            return HttpResponseRedirect('district')
+    else:
+        city_id = request.GET.get('id', "")
+        db = firestore.client()
+        details = db.collection('district').document(city_id).get()
+        cat = details.to_dict()
+    return render(request, 'district-edit.html', {"city": cat, "city_id": city_id})
+
+
+def delete_district(request):
+    data = request.POST
+    cat_id = data['id']
+    db = firestore.client()
+    db.collection('district').document(cat_id).delete()
+    cat_doc = db.collection("district").get()
+    category = []
+    for i in cat_doc:
+        d = i.to_dict()
+        d['id'] = i.id
+        category.append(d)
+    return render(request, 'district.html', {"cat": category})
+
+
+def show_grade(request):
+    fs = firestore.client()
+    cat_doc = fs.collection("grade").get()
+    city = []
+    for i in cat_doc:
+        d = i.to_dict()
+        d['id'] = i.id
+        city.append(d)
+    return render(request, 'grade.html', {"city": city})
+
+
+def add_grade(request):
+    if request.method == "POST":
+        data = request.POST
+        if 'name' in data:
+            db = firestore.client()
+            db.collection('grade').document().set({
+                "name": data['name']
+            })
+            return HttpResponseRedirect('grade')
+        else:
+            print("no fields")
+            return HttpResponseRedirect('grade')
+    else:
+        return render(request, 'grade-add.html')
+
+
+def edit_grade(request):
+    if request.method == "POST":
+        data = request.POST
+        if 'name' in data and 'id' in data:
+            db = firestore.client()
+            db.collection('grade').document(data['id']).update({
+
+                "name": data['name']
+            })
+            return HttpResponseRedirect('grade')
+        else:
+            return HttpResponseRedirect('grade')
+    else:
+        city_id = request.GET.get('id', "")
+        db = firestore.client()
+        details = db.collection('grade').document(city_id).get()
+        cat = details.to_dict()
+    return render(request, 'grade-edit.html', {"city": cat, "city_id": city_id})
+
+
+def delete_grade(request):
+    data = request.POST
+    cat_id = data['id']
+    db = firestore.client()
+    db.collection('grade').document(cat_id).delete()
+    cat_doc = db.collection("grade").get()
+    category = []
+    for i in cat_doc:
+        d = i.to_dict()
+        d['id'] = i.id
+        category.append(d)
+    return render(request, 'grade.html', {"cat": category})
+
+
+def show_user(request):
+    fs = firestore.client()
+    users_doc = fs.collection("users").get()
+    users = []
+    for i in users_doc:
+        users.append(i.to_dict())
+    return render(request, 'users.html', {"user_list": users})
