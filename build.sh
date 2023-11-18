f95ec51a2022ec864375416c5b106d9979a1a3b2
Sibiselva <63643348+Sibiselva-N@users.noreply.github.com>
2023-11-18 20:50:51 +0530

    latest

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 # Navigate to the Django project directory
 cd edhunt
 
+# Activate the virtual environment (if you're using one)
+# source path/to/your/venv/bin/activate
+
 # Install dependencies
 pip install -r requirements.txt
 
